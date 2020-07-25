44d0e01f7774f3ceb0ac219022ea78bd015b43d9
seolvov <hhhseolah@gmail.com>
2020-07-25 18:54:48 +0900

    fix: Interfaces 생성

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -2,22 +2,16 @@ package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
 import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
-import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController     //사실 이것도 Conponent의 일종  RestaurantController의 인스턴스를 만들어 준 적이 없지만 Spring이 관리했기 떄문에 정상적으로 작동함
+@RestController
 public class RestaurantController {
-/*
-    //의존성주입 하기 전
-    private RestaurantRepository repository = new RestaurantRepository();
-*/
-    @Autowired      //의존성주입 Contoller를 만들어 줄 때 Spring이 알아서 RestaurantRepository를 만들어 넣어줌
+    @Autowired
     private RestaurantRepository repository;
 
     @GetMapping("/restaurants")
@@ -28,37 +22,7 @@ public class RestaurantController {
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id){
-/*//첫번째 방법
-        Restaurant restaurant = null;
-        if(id == 1004L) {
-            restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-        }
-        if(id == 2020L) {
-            restaurant = new Restaurant(2020L, "Cyber Food", "Seoul");
-        }
-        return restaurant;
-*/
-/*//두번째 방법
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-*/
-/*
-//세번째 방법
-        List<Restaurant> restaurants = repository.findAll();
-*/
-
-//네번째 방법
         Restaurant restaurant = repository.findById(id);
-
-/*//원래 이런 기능을 Controller에서 하는 것이 아님
-        Restaurant restaurant = restaurants.stream()
-                .filter( r -> r.getId().equals(id))
-                .findFirst()
-                //.get();
-                .orElse(null);  //찾을 수 없는 경우 null로 해결
-*/
-
         return restaurant;
     }
 }
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,6 +1,7 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,27 +15,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)    //spring을 이용해서 Test를 실행할 수 있게 함
-@WebMvcTest(RestaurantController.class)   //특정 Controller(지금은 RestaurantController)를 Test할 거라는 것을 명시
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantController.class)
 public class RestaurantControllerTest {
-    @Autowired    //직접 만들지 않아도 Spirng에서 알아서 만들어 줄 수 있도록 처리
-    private MockMvc mvc;    //field생성
+    @Autowired
+    private MockMvc mvc;
 
-    //TEST를 하기 위해 Controller에 직접 의존성을 주입해야한다
-    @SpyBean    //Controller에 원하는 객체 주입가능   /없을 때는 EatgoApplication은 에러없이 실행됐지만 TEST는 에러발생했음
-    private RestaurantRepository restaurantRepository;  //이렇게 의존성 주입을 해주면 사용할 객체를 다양하게 변경할 수 있다는 것이 도움이 된다
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
 
     @Test
     public void list() throws Exception {
-        mvc.perform(get("/restaurants"))  //get("/restaurant") : 누구에게 요청할건지 처리 > @RunWith
-                .andExpect(status().isOk()) //요청을 하면 성공적인 결과가 나와야함
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")
                 ))
                 .andExpect(content().string(
                         containsString("\"name\":\"Bob zip\"")
                 ));
-        //
     }
 
     @Test
