cace4af143d5ad52ecf322fa045779219be060f5
seolvov <hhhseolah@gmail.com>
2020-08-05 21:31:36 +0900

    fix: RestaurantServiceTest.java에 @Mock사용

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -4,24 +4,54 @@ import kr.co.fastcampus.eatgo.domain.*;
 import kr.co.fastcampus.eatgo.domain.MenuItem;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
+
+    @Mock
     private RestaurantRepository restaurantRepository;
+
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     @Before //@Test 만 하면 Spring의 의존성주입이 안되므로 강제로 주입 / @Before는 모든 @Test가 실행되기 전에 반드시 실행
     public void setUp(){
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+        MockitoAnnotations.initMocks(this); //Mock Annotation 초기화
+
+        mockRestaurantRepository();
+        mockMenuItemRepository();
+
+        restaurantService = new RestaurantService(
+                restaurantRepository, menuItemRepository);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+
+        given(menuItemRepository.findAllByRestaurantId(1004L))
+                .willReturn(menuItems);
     }
 
     @Test
